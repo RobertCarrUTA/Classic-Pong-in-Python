ea5d16b16b471522bf664a8466e3a163223f3bd0
Robert Carr <robertcarrcisd@gmail.com>
2022-08-18 14:40:18 -0500

    Paddle B has been added
    Paddle B has been added to the window and is now present on the right hand side of the window.

--- pong.py
+++ pong.py
@@ -27,6 +27,15 @@ paddle_a.penup()
 paddle_a.goto(-350, 0) # Positions paddle to right side of window
 
 # Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+
+paddle_b.penup()
+
+paddle_b.goto(350, 0)
 
 # Ball
 
