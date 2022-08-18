c9c642a5bf2912ef7ef3a38208c18097cdb6add9
Robert Carr <robertcarrcisd@gmail.com>
2022-08-18 14:31:28 -0500

    Paddle A is now displayed
    Paddle A (the paddle on the left side of the screen) is now displayed in the window. It needs to be edited but it shows up.

--- pong.py
+++ pong.py
@@ -11,6 +11,21 @@ window.setup(width = 800, height = 600)
 # By manually updating it, it allows us to speed up the game
 window.tracer(0)
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) # sets the speed of the animation to the maximum possible speed, if not done, it would be really slow
+paddle_a.shape("square")
+paddle_a.color("white")
+
+# By definition, Turtle objects draw a line as they are moving. We do not want this and .penup() is how we remove the lines made when moving
+paddle_a.penup()
+
+paddle_a.goto(-350, 0) # Positions paddle to right side of window
+
+# Paddle B
+
+# Ball
+
 # Main game loop
 while True:
     window.update()
