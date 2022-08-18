d85e8fa9499285cd565a59a7dbb6dd1095128203
Robert Carr <robertcarrcisd@gmail.com>
2022-08-18 17:14:11 -0500

    Ball now speeds up
    The ball now speeds up after every collision with a paddle. It will reset its speed after going out of bounds and launch in the opposite direction of the boundary it went out on.

--- pong.py
+++ pong.py
@@ -106,20 +106,31 @@ while True:
     # If the ball goes off the right side, reset it to the center and reverse the direction
     if ball.xcor() > 390:
         ball.goto(0, 0)
-        ball.dx *= -1
+        ball.dx = -0.05
+
+        # We need to reset the ball speed if it goes out of bounds while still making it launch in the opposite direction
+        ball.dx = -0.05
+        ball.dy = -0.05
 
     # If the ball goes off the left side, reset it to the center and reverse the direction
     if ball.xcor() < -390:
         ball.goto(0, 0)
-        ball.dx *= -1
+        ball.dx = 0.05
+        ball.dy = 0.05
 
     # Collision Detection for Paddle A
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
 
+        # To increase the difficulty after every collision,
+        # we should increase the speed slightly
+        ball.dx *= 1.35
+        ball.dy *= 1.35
+
     # Collision Detection for Paddle B
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
-
+        ball.dx *= 1.35
+        ball.dy *= 1.35
