74efcbfb2eee4f45b98d9f1153adf05f3efa9bfa
Robert Carr <robertcarrcisd@gmail.com>
2022-08-18 17:25:37 -0500

    Player Scores Displayed (static)
    Player scores are now displayed on the screen but they do not change yet.

--- pong.py
+++ pong.py
@@ -53,6 +53,15 @@ ball.goto(0, 0)
 ball.dx = 0.05 # Movement in x direction
 ball.dy = -0.05 # Movement in y direction
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 # Paddle Movement Functions
 def paddle_a_up():
